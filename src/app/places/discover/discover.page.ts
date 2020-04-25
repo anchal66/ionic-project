a17b623751693b778bb7d02488798658a69bea02
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-25 16:26:42 +0530

    OPening menu proogramtically

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -10,10 +11,14 @@ import { Place } from '../place.model';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
-  constructor(private placesServices: PlacesService) { }
+  constructor(private placesServices: PlacesService, private menuContrl: MenuController) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesServices.getPlaces();
+  }
+
+  onOpenMenu(){
+    this.menuContrl.toggle('menu1')
   }
 
 }
