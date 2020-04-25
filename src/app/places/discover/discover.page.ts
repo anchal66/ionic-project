310f9b02774d60f62bcec0b224a69f738431a460
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-25 13:25:31 +0530

    Added Place service and displyed to HTML

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../places.service';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-discover',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverPage implements OnInit {
 
-  constructor() { }
+  loadedPlaces: Place[];
+  constructor(private placesServices: PlacesService) { }
 
   ngOnInit() {
+    this.loadedPlaces = this.placesServices.getPlaces();
   }
 
 }
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private _places: Place[] = [
+    new Place('p1',
+     'Avinash Nachal',
+      'in hearts',
+       'https://www.planetware.com/wpimages/2019/11/india-best-places-to-visit-agra.jpg',
+       90 ),
+       new Place('p2',
+     'Avijeet Nachal',
+      'in hearts',
+       'https://img.traveltriangle.com/blog/wp-content/uploads/2014/11/cover-for-Places-To-Visit-In-August-In-The-World.jpg',
+       80 ),
+       new Place('p3',
+     'Shrija Shree',
+      'in hearts',
+       'https://www.usnews.com/dims4/USNEWS/399cc03/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fcom-usnews-beam-media.s3.amazonaws.com%2F17%2F5c%2Fe08bd2a347be848e3dd4e98d88d9%2F2-30.%20San%20Francisco-Getty.jpg',
+       70 ),
+  ];
+
+  constructor() { }
+
+  getPlaces(){
+    return [...this._places];
+  }
+}
