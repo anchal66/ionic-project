9478eab40d4ac8f9a2594ca901f65d1877237c1d
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-26 12:00:10 +0530

    Added Segmented Buttons

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-discover',
@@ -22,6 +23,10 @@ export class DiscoverPage implements OnInit {
 
   onOpenMenu(){
     this.menuContrl.toggle('menu1')
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>){
+    console.log(event.detail)
   }
 
 }
