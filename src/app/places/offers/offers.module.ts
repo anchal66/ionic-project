f56742216de70b242453cc3aaa1a72c4f5814b9a
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-26 20:32:23 +0530

    Adding custom CSS

--- src/app/places/offers/offers.module.ts
+++ src/app/places/offers/offers.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { OffersPageRoutingModule } from './offers-routing.module';
 
 import { OffersPage } from './offers.page';
+import { OfferItemComponent } from './offer-item/offer-item.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { OffersPage } from './offers.page';
     IonicModule,
     OffersPageRoutingModule
   ],
-  declarations: [OffersPage]
+  declarations: [OffersPage,OfferItemComponent]
 })
 export class OffersPageModule {}
