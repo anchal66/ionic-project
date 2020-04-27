965adf3003970d45e512adb510ad3208306e4509
Avinash Anchal <anchalavinash66@gmail.com>
2020-04-27 10:59:28 +0530

    Impl edit offer to get place from param

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesService } from '../../places.service';
+import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
 
 @Component({
   selector: 'app-edit-offer',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor() { }
+  place: Place;
+  
+  constructor(private route: ActivatedRoute,
+    private placeService: PlacesService,
+    private navCtrl: NavController) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(param=>{
+      if(!param.has('placeId')){
+        this.navCtrl.navigateBack('/places/tabs/offers');
+        return
+      }
+      this.place = this.placeService.getPlace(param.get('placeId'));
+    })
   }
 
 }
